87249c29d61f353c599d31c12f4c4a57e5e14c4a
ContatoDevLuizOficial <122640710+ContatoDevLuiz@users.noreply.github.com>
2023-01-16 17:57:20 -0300

    Create Presentations.c

---
+++ Presentations.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+
+printf("Olá Boa Tarde, Muito Prazer Meu Nome É Luiz Marcos Abril Silva E Gostaria De Deixar Aqui Um Pouquinho  Da Minha História\n");
+printf("Gostaria Desde Já Agradecer Pela Recepitividade E Pela Simpatia Do Jovem Que Me Atendeu\n");
+printf("Acredito Que Seria Extremamente Produtivo Para A Minha Pessoa E Para A Empresa A Possibilidade De Ser Iniciado Na Empresa Como Um Trainne Justamente Por Não Possuir Vícios De Metodologias Operátivas De Outras Corporativas\n");
+printf("Sendo Assim Doutrinado E Adaptado Desde O Primeiro Momento Nas Metódicas Internas, E Seus Padrões Normativos\n");
+return 0;
+}
